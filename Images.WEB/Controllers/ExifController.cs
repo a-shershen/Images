32c7884d8faf23d9aae61473d65c70496e167cc6
andrei <accs.shershen@gmail.com>
2017-09-05 15:19:33 +0300

    Create Exif Data Controller, View

---
+++ Images.WEB/Controllers/ExifController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Images.WEB.Controllers
+{
+    public class ExifController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFile file)
+        {
+            if(file != null)
+            {
+
+            }
+        }
+
+    }
+}
