88f7d3b32e46f4d4fb2efe8c16bf2d947d97d60a
andrei <accs.shershen@gmail.com>
2017-09-05 15:19:09 +0300

    Hashing Library
    Create SHA256 Hash class

---
+++ Images.Hashing.Lib/Sha2Hash.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Images.Hashing.Lib
+{
+    public static class Sha2Hash
+    {
+        public static string GetHash(string str)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+
+            using (SHA256 sha = SHA256.Create())
+            {
+                bytes = sha.ComputeHash(bytes);
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (byte b in bytes)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+
+            return null;
+        }
+    }
+}
