1a114c6085c50c97427f09151c674b8075f560b2
andrei <accs.shershen@gmail.com>
2017-09-05 15:53:34 +0300

    Image.Web
    Update Exif Controller

--- Images.WEB/Controllers/ExifController.cs
+++ Images.WEB/Controllers/ExifController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Drawing;
 
 namespace Images.WEB.Controllers
 {
     public class ExifController : Controller
     {
+        private Images.Exif.Lib.Interfaces.IExifService exifService;
+
+        public ExifController(Images.Exif.Lib.Interfaces.IExifService iExifService)
+        {
+            exifService = iExifService;
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
@@ -15,13 +23,67 @@ namespace Images.WEB.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(HttpPostedFile file)
+        public ActionResult Upload()
         {
-            if(file != null)
-            {
+            HttpPostedFileBase file = Request.Files[0];
 
+            if (file != null)
+            {
+                
+                string ext;
+
+                switch (file.ContentType)
+                {
+                    case "image/jpeg":
+                        ext = ".jpg";
+                        break;
+                    case "image/png":
+                        ext = ".png";
+                        break;
+                    default:
+                        ViewBag.Error = true;
+                        ViewBag.ErrorMessage = "Неверный формат";
+                        return View();
+                }
+
+                string fileName
+                    = Images.Hashing.Lib.Sha2Hash.GetHash(file.FileName
+                    + DateTime.Now.ToLongDateString())
+                    + ext;
+
+                string serverPath = Server.MapPath("~/Images/Temp/")
+                   + fileName;
+
+                ViewBag.Path = "http://images.somee.com/Images/Temp/" + fileName;
+
+                IDictionary<string, string> dict;
+
+                using (Image img = Image.FromStream(file.InputStream))
+                {
+                    dict = exifService.GetProperties(img);
+                }
+
+                if (dict != null && dict.Count != 0)
+                {
+                    ViewBag.WithExif = true;
+                }
+
+                else
+                {
+                    ViewBag.WithExif = false;
+                }
+
+
+                file.SaveAs(serverPath);
+                file.InputStream.Dispose();
+
+                return View("Index", dict);
+            }
+
+            else
+            {
+                return View();
             }
         }
-
     }
 }
---
+++ Images.WEB/DependencyResolvers/CustomResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+
+namespace Images.WEB.DependencyResolvers
+{
+    public class CustomResolver : System.Web.Mvc.IDependencyResolver
+    {
+        private Ninject.IKernel kernel;
+
+        public CustomResolver(Ninject.IKernel iKernel)
+        {
+            kernel = iKernel;
+            Binds();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void Binds()
+        {
+            kernel.Bind<Images.Exif.Lib.Interfaces.IExifService>()
+                .To<Images.Exif.Lib.Services.ExifService>();
+        }
+    }
+}
